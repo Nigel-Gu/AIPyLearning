ebe6dfe9ddc50577957b6716b72cdd0289fc292e
nigel.gu <nigelwajj@163.com>
2024-04-19 14:37:10 +0800

    test push

--- Test.py
+++ Test.py
@@ -1,9 +1,13 @@
-import torch
-import random
+import torch as th
+import random as rd
+import numpy as np
 
-x=torch.rand(5,3)
-y=random
 
+x=th.rand(5,3)
+print(x)
+y=rd.randint(1,20)
+print(y)
+y=np.array(y)
+x=x+y
 
 print(x)
-print(y)
